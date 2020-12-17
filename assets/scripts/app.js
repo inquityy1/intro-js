dfe8dbe74937ecd2e0c0f997143f350b4ea6b37d
inquityy1 <nebojsanikolic12350@yahoo.com>
2020-12-17 08:02:23 +0100

    all buttons works and shortcuts += -=...

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,46 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+// gets input from input field
 function getUserNumberInput() {
-	return parseInt(userInput.value);
+	return parseInt(usrInput.value);
+}
+
+// generates and writes calculation log
+function  createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+	const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+	outputResult(currentResult, calcDescription); //from vendor file
 }
 
 function add() {
-	const enteredNumber = getUserNumberInput;
-	const calcDescription = `${currentResult} + ${enteredNumber}`;
-	currentResult = currentResult + enteredNumber;
-	outputResult(currentResult, calcDescription);
+	const enteredNumber = getUserNumberInput();
+	const initialResult = currentResult;
+	currentResult += enteredNumber;
+	createAndWriteOutput('+', initialResult, enteredNumber)
+}
+
+function subtract() {
+	const enteredNumber = getUserNumberInput();
+	const initialResult = currentResult;
+	currentResult -= enteredNumber;
+	createAndWriteOutput('-', initialResult, enteredNumber)
+}
+
+function multiply() {
+	const enteredNumber = getUserNumberInput();
+	const initialResult = currentResult;
+	currentResult *= enteredNumber;
+	createAndWriteOutput('*', initialResult, enteredNumber)
+}
+
+function divide() {
+	const enteredNumber = getUserNumberInput();
+	const initialResult = currentResult;
+	currentResult /= enteredNumber;
+	createAndWriteOutput('/', initialResult, enteredNumber)
 }
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
